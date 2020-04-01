cf096efb3857663ce309194f2ccca3ca5bbd8304
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 18:42:33 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,8 +17,7 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  matrix = [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
-  return matrix
+  [CONVENTIONAL_PRODUCE, ORGANIC_PRODUCE]
 end
 
 def sorted_matrix 
