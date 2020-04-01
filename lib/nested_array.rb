7806203c64d411906150d48f096ecab3a9feb05b
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-01 18:34:26 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -17,15 +17,15 @@ CONVENTIONAL_PRODUCE = [
 ]
 
 def assembled_matrix
-  
+  puts [ORGANIC_PRODUCE, CONVENTIONAL_PRODUCE]
 end
 
-def sorted_matrix = assembled_matrix.map { |arr|  arr.sort { |a, b| a <=> b }}
+def sorted_matrix 
+  puts assembled_matrix.map { |arr|  arr.sort { |a, b| a <=> b }}
 end
 
 def matrix_lookup(matrix, row, column)
-  # Given any matrix (array of arrays), a row index and a column index, 
-  # Return the matrix's content at that row and and column
+  puts matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
